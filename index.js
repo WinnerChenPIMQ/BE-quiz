e9af06a3af4d002944e18a4b2713699aee52695f
Winner Chen <79506825+WinnerChenPIMQ@users.noreply.github.com>
2024-03-07 15:41:55 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Implement a function to return duplicated number in the array
+ * If there is none, return -1
+ * @params (number[]) input
+ * @return number
+ */
+function solve(input) {
+
+}
+
+
+console.log(solve([1,2,3,1,4,5]));  // 1
+console.log(solve([3,2,3,1,4,5]));  // 3
+console.log(solve([1,2,3,11,4,5]));  // -1
